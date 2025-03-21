dff134b4ea57f1ae8ab3d86d992458a636fb6fe9
depidsvy <spidgorny@gmail.com>
2025-03-21 23:25:25 +0100

    fix: redirect error

--- lib/get-locale.tsx
+++ lib/get-locale.tsx
@@ -1,12 +1,16 @@
 import Negotiator from "negotiator";
 import { match } from "@formatjs/intl-localematcher";
 
+let defaultLocale = "en";
 let locales = ["en", "de"];
 
 export function getLocale(headers: object) {
 	let languages = new Negotiator({ headers }).languages();
 	console.log("languages", languages);
-	let defaultLocale = "en";
+
+	if (["*"].toString() === languages.toString()) {
+		return defaultLocale;
+	}
 
 	return match(languages, locales, defaultLocale); // -> 'en-US'
 }
